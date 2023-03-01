516053c28d79c36a59e5d2cca306126ec63fd43c
DreamBoy65 <rajpootpratham904@gmail.com>
2023-03-01 18:15:48 +0530

    update

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,9 +1,5 @@
 const slashHandler = require("../handlers/slashCommands.js")
-const {
-  handleTicketSystem
-} = require("../handlers/ticketHandler.js")
 
 module.exports = async(client, int) => {
   await slashHandler(client, int)
-  await handleTicketSystem(client, int)
 }
